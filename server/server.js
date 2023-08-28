4dc6792aac6142916668839890a25f15881f52a6
Jake <55953689+jakecreely@users.noreply.github.com>
2023-08-28 21:30:55 +1000

    Check for env before loading

--- server/server.js
+++ server/server.js
@@ -21,8 +21,9 @@ const roleRoutes = require('./routes/roleRoutes')
 const documentRoutes = require('./routes/documentRoutes');
 const { default: axios } = require("axios");
 const env = process.env.NODE_ENV || 'development';
-
-dotenv.config({ path: `./.env.${env}` });
+if (env !== 'production') {
+  dotenv.config({ path: `./.env.${env}` });
+}
 let connection;
 
 const setupServer = () => {
